bcb17edc0144c2cba08b75b1957ccadc876e4c67
dYoshitakaKato <phoenix1331019@gmail.com>
2021-07-23 23:00:11 +0900

    Contributors一覧Fetch処理

---
+++ app/src/test/java/com/example/contributors/TestContributorsReposiitory.kt
@@ -0,0 +1,62 @@
+package com.example.contributors
+
+import com.example.contributors.repository.Contributor
+import com.example.contributors.repository.ContributorsRepository
+import org.junit.Assert
+import org.junit.Test
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
+
+class TestContributorsReposiitory {
+    @Test
+    fun fetchSuccessTest() {
+        val repository = ContributorsRepository("https://api.github.com")
+        val countDownLatch = CountDownLatch(1)
+        var contributors: List<Contributor> = ArrayList<Contributor>()
+        repository.createService().fetch().enqueue(object : Callback<List<Contributor>> {
+            override fun onResponse(call: Call<List<Contributor>>, response: Response<List<Contributor>>) {
+                if (!response.isSuccessful) {
+                    countDownLatch.countDown()
+                    return
+                }
+                response.body()?.let {
+                    contributors = it
+                    countDownLatch.countDown()
+                }
+            }
+            override fun onFailure(call: Call<List<Contributor>>, t: Throwable) {
+                Assert.fail("Fetch失敗")
+                return
+            }
+        })
+        Assert.assertTrue("非同期タイムアウト", countDownLatch.await(1, TimeUnit.SECONDS))
+        Assert.assertFalse("取得エラー", contributors.isEmpty())
+        Assert.assertEquals("取得件数エラー", contributors.size, 30)
+    }
+
+    @Test
+    fun fetchFaildTest() {
+        val repository = ContributorsRepository("https://xxxxxxxxxxxxxxxxx")
+        val countDownLatch = CountDownLatch(1)
+        var contributors: List<Contributor> = ArrayList<Contributor>()
+        repository.createService().fetch().enqueue(object : Callback<List<Contributor>> {
+            override fun onResponse(call: Call<List<Contributor>>, response: Response<List<Contributor>>) {
+                if (response.isSuccessful) {
+                    response.body()?.let {
+                        contributors = it
+                    }
+                }
+                Assert.fail("Fetch成功")
+            }
+            override fun onFailure(call: Call<List<Contributor>>, t: Throwable) {
+                countDownLatch.countDown()
+                return
+            }
+        })
+        Assert.assertTrue("非同期タイムアウト", countDownLatch.await(1, TimeUnit.SECONDS))
+        Assert.assertTrue("取得エラー", contributors.isEmpty())
+    }
+}
