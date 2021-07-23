7f7a4fcef4c5bc71f58f3384e33440a0c105d70a
dYoshitakaKato <phoenix1331019@gmail.com>
2021-07-24 04:15:04 +0900

    ContributorsRepositoryをViewModelへDIする

--- app/src/main/java/com/example/contributors/viewModel/MainViewModel.kt
+++ app/src/main/java/com/example/contributors/viewModel/MainViewModel.kt
@@ -1,10 +1,13 @@
 package com.example.contributors.viewModel
 
 import androidx.lifecycle.ViewModel
+import com.example.contributors.repository.ContributorsRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel @Inject constructor()  : ViewModel() {
-    // TODO: Implement the ViewModel
+class MainViewModel @Inject constructor(private val repository: ContributorsRepository)  : ViewModel() {
+    fun load(){
+        repository.createService().fetch()
+    }
 }
