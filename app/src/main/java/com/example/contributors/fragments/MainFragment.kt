0e76c0e798083471633e9d6df426e0b3467a491b
dYoshitakaKato <phoenix1331019@gmail.com>
2021-07-24 02:07:04 +0900

    Hiltの登録

--- app/src/main/java/com/example/contributors/fragments/MainFragment.kt
+++ app/src/main/java/com/example/contributors/fragments/MainFragment.kt
@@ -1,21 +1,25 @@
 package com.example.contributors.fragments
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.contributors.R
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.contributors.databinding.MainFragmentBinding
+import com.example.contributors.viewModel.ContributorItemViewModel
 import com.example.contributors.viewModel.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainFragment : Fragment() {
 
     companion object {
         fun newInstance() = MainFragment()
     }
 
-    private lateinit var viewModel: MainViewModel
+    private val viewModel by viewModels<MainViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,5 +33,4 @@ class MainFragment : Fragment() {
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         // TODO: Use the ViewModel
     }
-
 }
